ddd195819a76ea610745ee4676ff862a9c5cba7f
BlahGeek <i@BlahGeek.com>
2015-07-22 15:32:17 +0800

    add bash script

---
+++ clients/bash/hpshare.bash
@@ -0,0 +1,66 @@
+#!/bin/bash -e
+
+USAGE="Usage: hpshare [OPTIONS] file.ext
+    -u, --user:    username
+    -p, --private: use longer URL
+    -s, --server:  server host name, default to blaa.ml"
+
+SERVER="blaa.ml"
+USERNAME="$(whoami | tr [:upper:] [:lower:])"
+
+hash jsawk 2>/dev/null || { echo "jsawk(https://github.com/micha/jsawk) not installed."; exit 1; }
+
+while [[ $# > 0 ]]
+do
+    key="$1"
+    case $key in
+        -u|--user)
+            USERNAME="$2"
+            shift
+            ;;
+        -p|--private)
+            PRIVATE=true
+            ;;
+        -s|--server)
+            SERVER="$2"
+            shift
+            ;;
+        -h|--help)
+            echo "$USAGE"
+            exit 0
+            ;;
+        *)
+            FILE="$1"
+            ;;
+    esac
+    shift
+done
+
+if [[ ! -f $FILE ]]; then
+    echo "$USAGE"
+    exit 1
+fi
+
+PERMIT_OUTPUT=$(curl -s -X POST -u "$USERNAME" \
+                "http://$SERVER/permit/" \
+                -d "filename=$FILE" \
+                -d "private=$PRIVATE")
+TOKEN=$(echo $PERMIT_OUTPUT | jsawk "return this.token")
+KEY=$(echo $PERMIT_OUTPUT | jsawk "return this.key")
+
+if [[ -z $TOKEN ]]; then
+    echo "Permit error."
+    exit 1
+fi
+echo "Uploading to $KEY..."
+
+UPLOAD_OUTPUT=$(curl --progress-bar "http://up.qiniu.com" \
+                -F token="$TOKEN" \
+                -F key="$KEY" \
+                -F "file=@$FILE")
+URL=$(echo $UPLOAD_OUTPUT | jsawk "return this.url")
+if [[ -n $URL ]]; then
+    echo "Upload done, URL: $URL"
+else
+    echo "Upload error: $UPLOAD_OUTPUT"
+fi
