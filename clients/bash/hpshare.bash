982a99640b2b582ec541145be2762756a0ef0ca8
BlahGeek <i@BlahGeek.com>
2015-08-04 23:38:10 +0800

    update bash client: support grouping

--- clients/bash/hpshare.bash
+++ clients/bash/hpshare.bash
@@ -10,6 +10,8 @@ USERNAME="$(whoami | tr [:upper:] [:lower:])"
 
 hash jsawk 2>/dev/null || { echo "jsawk(https://github.com/micha/jsawk) not installed."; exit 1; }
 
+FILES=()
+
 while [[ $# > 0 ]]
 do
     key="$1"
@@ -30,35 +32,59 @@ do
             exit 0
             ;;
         *)
-            FILE="$1"
+            FILES+=("$1")
             ;;
     esac
     shift
 done
 
-if [[ ! -f $FILE ]]; then
+echo -n "Password for $USERNAME@$SERVER:"
+read -s PASSWORD
+echo
+
+IDS=""
+
+if [[ ${#FILES[*]} -eq 0 ]]; then
     echo "$USAGE"
     exit 1
 fi
 
-PERMIT_OUTPUT=$(curl -s -X POST -u "$USERNAME" \
-                "http://$SERVER/permit/" \
-                -d "filename=$FILE" \
-                -d "private=$PRIVATE")
-TOKEN=$(echo $PERMIT_OUTPUT | jsawk "return this.token")
+for FILE in ${FILES[*]}
+do
+    echo "Uploading $FILE..."
+    PERMIT_OUTPUT=$(curl -s -X POST -u "$USERNAME:$PASSWORD" \
+                    "http://$SERVER/permit/" \
+                    -d "filename=$FILE" \
+                    -d "private=$PRIVATE")
+    TOKEN=$(echo $PERMIT_OUTPUT | jsawk "return this.token")
 
-if [[ -z $TOKEN ]]; then
-    echo "Permit error."
-    exit 1
-fi
-echo "Uploading..."
+    if [[ -z $TOKEN ]]; then
+        echo "Permit error."
+        continue
+    fi
 
-UPLOAD_OUTPUT=$(curl --progress-bar "http://up.qiniu.com" \
-                -F token="$TOKEN" \
-                -F "file=@$FILE")
-URL=$(echo $UPLOAD_OUTPUT | jsawk "return this.url")
-if [[ -n $URL ]]; then
-    echo "Upload done, URL: $URL"
-else
-    echo "Upload error: $UPLOAD_OUTPUT"
+    UPLOAD_OUTPUT=$(curl --progress-bar "http://up.qiniu.com" \
+                    -F token="$TOKEN" \
+                    -F "file=@$FILE")
+    URL=$(echo $UPLOAD_OUTPUT | jsawk "return this.url")
+    ID=$(echo $UPLOAD_OUTPUT | jsawk "return this.id")
+    if [[ -n $URL ]]; then
+        echo "Upload done, URL: $URL"
+        echo
+        IDS="$IDS,$ID"
+    else
+        echo "Upload error: $UPLOAD_OUTPUT"
+    fi
+done
+
+IDS=${IDS:1:${#IDS}}
+
+if [[ ${#FILES[*]} -gt 1 ]]; then
+    echo "Grouping..."
+    GROUP_OUTPUT=$(curl -s -X POST -u "$USERNAME:$PASSWORD" \
+                   "http://$SERVER/newgroup/" \
+                   -d "ids=$IDS" -d "private=$PRIVATE")
+    URL=$(echo $GROUP_OUTPUT | jsawk "return this.url")
+    COUNT=$(echo $GROUP_OUTPUT | jsawk "return this.count")
+    echo "$COUNT files in group, URL: $URL"
 fi
